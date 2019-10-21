6d08d2da88edec8e24c56ccc47928d64d5fb95ea
Alejo Schreyer <rusetobalsamico1@gmail.com>
2019-10-21 17:22:27 -0300

    Create Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './App.css';
+// importo el componente Header.js
+import Header from './Header.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,20 @@
+// importamos REACT
+import React from 'react';
+import './Header.css';
+
+// Creo el modulo Header
+function Header() {
+	return(
+		<header>
+			<ul>
+				<li>Home</li>
+				<li>Contacto</li>
+			</ul>
+		</header>
+	);
+};
+
+export default Header;
+
+
+
